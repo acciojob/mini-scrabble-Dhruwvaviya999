6f585840bc86f1cd5af190a8bc2cb67dc716f718
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-09-19 11:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+
+const textBox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+textBox.oninput = (e) => {
+    letterCount.innerText = e.target.value.length;
+    console.log(e.target.value);
+}
